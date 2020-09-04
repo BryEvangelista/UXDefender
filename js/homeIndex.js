a2223cc5f4931046f4bfb1fd6093900d33b33334
BryEvangelista <63068718+BryEvangelista@users.noreply.github.com>
2020-09-04 17:20:30 -0500

    added java script to homepage

---
+++ js/homeIndex.js
@@ -0,0 +1,11 @@
+
+
+var UXDefenders = 'Hello, Your homeIndex.js file is loaded correctly! Enjoy!'
+
+console.log(UXDefenders)
+
+function fade() {
+    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
+    
