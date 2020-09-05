31fa6de1e1de2c52cfdbcf73a8a4523ac49f952b
rileyportergithub <rileyporterjob@gmail.com>
2020-09-04 22:06:00 -0700

    Shaking icons and email alert
    shaking icons and Alert feature on email

---
+++ js/footerIndex.js
@@ -0,0 +1,18 @@
+
+
+<script
+			  src="https://code.jquery.com/jquery-3.5.1.min.js"
+			  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+			  crossorigin="anonymous"></script>
+
+<script>
+$(document).ready(function(){
+
+$(".flexbox-item-1").on("click", function() {
+effect( "shake", {times:4}, 1000 );
+
+});
+
+});
+</script>
+
