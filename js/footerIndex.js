37162cea49d59075f9c25d2cb8d5d4f00f3011d5
BryEvangelista <63068718+BryEvangelista@users.noreply.github.com>
2020-09-07 12:51:43 -0500

    homepage edits final

--- js/footerIndex.js
+++ js/footerIndex.js
@@ -1,9 +1,8 @@
 
-
-/*<script
+<script
 			  src="https://code.jquery.com/jquery-3.5.1.min.js"
 			  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-			  crossorigin="anonymous"></script>*/
+			  crossorigin="anonymous"></script>
 
 
 $(document).ready(function(){
